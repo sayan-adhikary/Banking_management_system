1a5456b3346e9fa6ebc893e6d62696851f84c2ca
sayan-adhikary <pap9679429830@gmail.com>
2025-01-11 20:54:23 +0530

    Updated

--- src/bank/management/system/Connn.java
+++ src/bank/management/system/Connn.java
@@ -7,7 +7,11 @@ public class Connn {
     Statement statement;
     public Connn(){
         try{
+<<<<<<< HEAD
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/banksystem","root","Sayan@123");
+=======
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","AyushVish");
+>>>>>>> 471959db23a560b752a60583527e6fb5f7d5e842
             statement = connection.createStatement();
         }catch (Exception e){
             e.printStackTrace();
